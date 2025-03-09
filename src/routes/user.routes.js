1cea1259e021435c0f906414e175704dd64d524c
Khushi Shah <khushibshah06@gmail.com>
2025-03-09 19:54:18 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, 
+        getCurrentUser,
+        getUserChannelProfile,
+        getWatchHistory,
+        logoutUser,
+        registerUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImage } 
+        from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
 import { loginUser } from "../controllers/user.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";//for verifying if user is loggedin
 import { refreshAccessToken } from "../controllers/user.controller.js";
 
  const router = Router()
@@ -26,4 +35,16 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
- export default router
+//update routes
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)//as we want to take details from user
+router.route("/current-user").get(verifyJWT,getCurrentUser)//as nothing to take from user
+router.route("update-account").patch(verifyJWT,updateAccountDetails)//as we want to update existing details
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)//multer upload method to get file
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)//as data is taken from url
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+export default router
